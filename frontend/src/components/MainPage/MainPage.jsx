a3301abbae0c27fabf1e2f1598d4eed73590f343
Max <mfong415@gmail.com>
2023-01-15 17:47:38 -0800

    Added boilerplate frontend code

---
+++ frontend/src/components/MainPage/MainPage.jsx
@@ -0,0 +1,13 @@
+
+function MainPage() {
+    return (
+      <>
+        <p>Singed</p>
+        <footer>
+          Copyright &copy; 2022 Singed
+        </footer>
+      </>
+    );
+  }
+  
+  export default MainPage;
