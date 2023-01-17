bdb000bac5f691c66cb759e2d9a1b589963b080e
ByronHan333 <zhan12@dons.usfca.edu>
2023-01-17 14:44:31 -0800

    update init seed message

--- frontend/src/components/Map/index.jsx
+++ frontend/src/components/Map/index.jsx
@@ -17,10 +17,11 @@ export default function Map() {
   return <MapContainer restaurants={Object.values(restaurants)} center={{lat:37.773972, lng:-122.431297}}/>
 }
 
-export function PriceCard({num, onClick}) {
+export function PriceCard({restaurant, onClick}) {
+  console.log(restaurant.rating)
   return (
     <div className="map-price-card cursor" onClick={onClick}>
-      <p>Star: {num}</p>
+      <p>Star: {restaurant.rating}</p>
     </div>
   )
 }
@@ -31,8 +32,8 @@ export function MapContainer({restaurants, center}) {
     googleMapsApiKey: process.env.REACT_APP_MAPS_API_KEY
   });
 
-  console.log(restaurants)
-  
+  // console.log(restaurants)
+
   const centerM = useMemo(() => (center), [])
   const history = useHistory()
 
@@ -46,9 +47,17 @@ export function MapContainer({restaurants, center}) {
     <>
       <GoogleMap zoom={11} center={centerM} mapContainerClassName="map-container" options={{styles: styles}}>
         {restaurants.map(restaurant => {
+          console.log({lat:restaurant.longitude, lng:restaurant.latitude})
+          // console.log(typeof restaurant.longitude)
+          console.log({lat:37.773972, lng:-122.431297})
+          // console.log(typeof 37.773972)
           return (
-            <OverlayView key = {restaurant._id} position={{lat:restaurant.latitude, lng:restaurant.longitude}} mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}>
-              <PriceCard restaurant={restaurant} onClick={(e) => priceCardOnClick(e, restaurant)} />
+            // <OverlayView key = {restaurant._id} position={{lat:37.773972, lng:-122.431297}} mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}>
+            <OverlayView key = {restaurant._id} position={{lat:restaurant.longitude, lng:restaurant.latitude}} mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}>
+              {/* <PriceCard restaurant={restaurant} onClick={(e) => priceCardOnClick(e, restaurant)} /> */}
+              <div style={{ background: `white`, border: `1px solid #ccc`, padding: 15 }}>
+                <h1>OverlayView</h1>
+              </div>
             </OverlayView>
           )
         })}
