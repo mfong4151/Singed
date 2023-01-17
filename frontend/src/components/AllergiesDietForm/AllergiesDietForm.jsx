44947d62ef105047d17866c238e280cd0765bb2f
Patrick Choi <ptrckchoi@gmail.com>
2023-01-17 15:04:40 -0800

    basic allergies css, will add footers in this branch too

--- frontend/src/components/AllergiesDietForm/AllergiesDietForm.jsx
+++ frontend/src/components/AllergiesDietForm/AllergiesDietForm.jsx
@@ -1,6 +1,13 @@
 import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import "./AllergiesDietForm.css"
+import shellfish from "../../assets/survey_imgs/shellfish_cropped.jpg"
+import fish from "../../assets/survey_imgs/fish_cropped.jpg"
+import gluten from "../../assets/survey_imgs/gluten.jpg"
+import lactose from "../../assets/survey_imgs/lactose.jpg"
+import nuts from "../../assets/survey_imgs/nuts_cropped.jpg"
+import vegan from "../../assets/survey_imgs/vegan.jpg"
+
 
 function AllergiesDietForm() {
     // const sessionUser = useSelector((store) => store.session.user);
@@ -38,56 +45,78 @@ function AllergiesDietForm() {
     }
 
     return (
-        <div className="allergies-diet-form-parent">
+        // <div className="allergies-diet-form-parent">
             <div className="allergies-diet-form-container">
                 <form className="allergies-diet-form" onSubmit={handleSubmit}>
                     <div className="allergies-form">
-                        <p>Allergies</p>
-                        <label>Shellfish
-                            <input 
-                                type="checkbox"
-                                checked={shellfishAllergy}
-                                onChange={(e) =>{setShellfishAllergy(e.target.checked)}} />
-                        </label>
-                        <label>Fish
-                            <input 
-                                type="checkbox"
-                                checked={fishAllergy}
-                                onChange={(e) =>{setFishAllergy(e.target.checked)}} />
-                        </label>
-                        <label>Nuts
-                            <input 
-                                type="checkbox"
-                                checked={nutsAllergy}
-                                onChange={(e) =>{setNutsAllergy(e.target.checked)}} />
-                        </label>
-
+                        <h2>Allergies</h2>
+                        <div className="allergies-inputs">
+                            
+                            <div className="survey-input">
+                                <label>Shellfish 
+                                    <img src={shellfish}/>
+                                    <input
+                                        type="checkbox"
+                                        checked={shellfishAllergy}
+                                        onChange={(e) =>{setShellfishAllergy(e.target.checked)}} />
+                                </label>
+                            </div>
+                            <div className="survey-input">
+                                <label>Fish
+                                <img src={fish}/>
+                                    <input
+                                        type="checkbox"
+                                        checked={fishAllergy}
+                                        onChange={(e) =>{setFishAllergy(e.target.checked)}} />
+                                </label>
+                            </div>
+                            <div className="survey-input">
+                                <label>Nuts
+                                <img src={nuts}/>
+                                    <input
+                                        type="checkbox"
+                                        checked={nutsAllergy}
+                                        onChange={(e) =>{setNutsAllergy(e.target.checked)}} />
+                                </label>
+                            </div>
+                        </div>
                     </div>
                     <div className="diet-form">
-                        <p>Dietary Restrictions</p>
-                        <label>Vegan
-                            <input 
-                                type="checkbox"
-                                checked={veganDiet}
-                                onChange={(e) =>{setVeganDiet(e.target.checked)}} />
-                        </label>
-                        <label>Gluten
-                            <input 
-                                type="checkbox"
-                                checked={glutenDiet}
-                                onChange={(e) =>{setGlutenDiet(e.target.checked)}} />
-                        </label>
-                        <label>Lactose
-                            <input 
-                                type="checkbox"
-                                checked={lactoseDiet}
-                                onChange={(e) =>{setLactoseDiet(e.target.checked)}} />
-                        </label>
+                        <h2>Dietary Restrictions</h2>
+                        <div className="dietary-inputs">
+                            <div className="survey-input">
+                                <label>Vegan
+                                <img src={vegan}/>
+                                    <input
+                                        type="checkbox"
+                                        checked={veganDiet}
+                                        onChange={(e) =>{setVeganDiet(e.target.checked)}} />
+                                </label>
+                            </div>
+                            <div className="survey-input">
+                                <label>Gluten
+                                <img src={gluten}/>
+                                    <input
+                                        type="checkbox"
+                                        checked={glutenDiet}
+                                        onChange={(e) =>{setGlutenDiet(e.target.checked)}} />
+                                </label>
+                            </div>
+                            <div className="survey-input">
+                                <label>Lactose
+                                <img src={lactose}/>
+                                    <input
+                                        type="checkbox"
+                                        checked={lactoseDiet}
+                                        onChange={(e) =>{setLactoseDiet(e.target.checked)}} />
+                                </label>
+                            </div>
+                        </div>
                     </div>
-                    <button type="submit">Submit</button>
+                    <button type="submit">Next</button>
                 </form>
             </div>
-        </div>
+        // </div>
     )
 
 }
