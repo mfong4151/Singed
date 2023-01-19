e97c82b1f79358835e25c0a771490748e9f31a41
Patrick Choi <ptrckchoi@gmail.com>
2023-01-19 10:58:22 -0800

    started refactoring messenger

--- frontend/src/components/MainPage/MessengerModal/MessengerModal.jsx
+++ frontend/src/components/MainPage/MessengerModal/MessengerModal.jsx
@@ -1,20 +1,42 @@
 import React from 'react'
 import MessageForm from './MessegeForm'
 import {useSelector} from 'react-redux';
+
+export const openCommunityModal = () => {
+
+  document.getElementById("modal-overlay").style.width = "100vw";
+  document.getElementById("messenger-show-body").style.width = "250px";
+  // document.getElementById("modal-menu-content").style.display = "inline";
+  document.getElementById("messenger-show-body").style.padding = "20px";
+
+  let children = document.querySelectorAll('#messenger-show-body > *'); //this workaround is bc my children does not disappear when modal is gone
+  for (let i = 0; i < children.length; i++) {
+      children[i].style.display = "inline";
+  }
+}
+export const closeCommunityModal = () => {
+  document.getElementById("modal-overlay").style.width = "0";
+  document.getElementById("messenger-show-body").style.width = "0";
+  document.getElementById("messenger-show-body").style.padding = "0";
+  let children = document.querySelectorAll('#messenger-show-body > *');
+  for (let i = 0; i < children.length; i++) {
+      children[i].style.display = "none";
+  }
+}
+
 const MessengerModal = ({messengerModal, setMessengerModal}) => {
 
   // const chats = useSelector(state => state)
       
-  if (messengerModal) document.body.classList.add('active-modal')
-  else document.body.classList.remove('active-modal')
+  // if (messengerModal) document.body.classList.add('active-modal')
+  // else document.body.classList.remove('active-modal')
 
+  
 
   return (
         <div>
-            <div className='modal-overlay' onClick={()=>setMessengerModal(!messengerModal)}>
-
+            <div id='modal-overlay-chat' onClick={()=>setMessengerModal(!messengerModal)}>
                 <div className='messenger-univ' id='messenger-show-body' onClick={e => e.stopPropagation()}>
-
                   <div id="my-chatrooms">
                     
                       {/* <ul>
@@ -53,8 +75,6 @@ const MessengerModal = ({messengerModal, setMessengerModal}) => {
                       } */}
                     </ul>
                     <MessageForm/>
-
-
                   </div>
                </div>
         </div>
