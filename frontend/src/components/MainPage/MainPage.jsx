683d60f6f37f2376bd0ee3c94813c9d3d81939d0
bob-skywalker <q463250938@gmail.com>
2023-01-19 13:08:36 -0800

    slight work

--- frontend/src/components/MainPage/MainPage.jsx
+++ frontend/src/components/MainPage/MainPage.jsx
@@ -1,13 +1,24 @@
 import Map from "../Map";
 import MainPageRestaurants from './MainPageRestaurants'
 import './MainPage.css'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MessengerModalTab from "./MessengerModal/MessengerModalTab";
 import MessengerModal from "./MessengerModal/MessengerModal";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchGroups } from "../../store/group";
 
 export default function MainPage() {
   //Added to manage the messenger
-  const [messengerModal, setMessengerModal] = useState(false)
+  const [messengerModal, setMessengerModal] = useState(false);
+  const sessionUser = useSelector((store) => store.session.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if(sessionUser){
+      dispatch(fetchGroups())
+    }
+
+  }, )
 
 
 
--- frontend/src/components/MainPage/MessengerModal/MessengerModal.jsx
+++ frontend/src/components/MainPage/MessengerModal/MessengerModal.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import MessageForm from './MessegeForm'
 import {useSelector} from 'react-redux';
-const MessengerModal = ({messengerModal, setMessengerModal}) => {
+import { useParams } from 'react-router-dom';
 
-  // const chats = useSelector(state => state)
+const useChatScroll = (dep) => {
+  const ref = useRef(null);
+  useEffect(() => {
+    if(ref.current){
+      ref.current.scrollTop = ref.current.scrollHeight;
+    }
+  }, [dep])
+  return ref
+}
+const MessengerModal = ({messengerModal, setMessengerModal}) => {
+  const {groupId} = useParams();
+  const group = useSelector((store) => store.groups[groupId])
+
       
   if (messengerModal) document.body.classList.add('active-modal')
   else document.body.classList.remove('active-modal')
