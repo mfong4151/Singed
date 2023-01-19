1eb1a347e0a28a47fd6b13c8ff38b036e48de364
Daphne Lam <remnantsofred@gmail.com>
2023-01-19 11:31:51 -0800

    Implemented community modal

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -11,10 +11,6 @@ function Footer () {
        
         <div className='footer-left'>
         </div>
-        <div className="website-info">
-          <h3>About Us</h3>
-          <p>Welcome! Singe is a MERN fullstack application made by 4 aspiring web developers. Blah blah blah</p>
-        </div>
         <div className='contacts'>
           <h3>Contact Us!</h3>
           <ul>          
--- frontend/src/components/NavBar/Navbar.jsx
+++ frontend/src/components/NavBar/Navbar.jsx
@@ -1,30 +1,29 @@
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import './NavBar.css';
 import { logout } from '../../store/session';
 import power from '../../assets/power_happy.png';
+import CommunityModal from './CommunityModal/CommunityModal';
+import {FiMenu} from 'react-icons/fi'
+import {openCommunityModal} from './CommunityModal/CommunityModal.jsx'
 
 function NavBar () {
   const loggedIn = useSelector(state => !!state.session.user);
   const dispatch = useDispatch();
-  const history = useHistory();
+
 
   const logoutUser = e => {
       e.preventDefault();
       dispatch(logout());
   }
-
-  const handleLogoClick = (e) => {
-    e.preventDefault();
-    console.log('logo clicked')
-    history.push('/')
-  }
-
   const getLinks = () => {
     if (loggedIn) {
       return (
         <div className="navbar">
-          <div className='navbar-left' onClick={handleLogoClick}>
+          <div className='navbar-left'>
+            <div className="community-menu-icon" onClick={openCommunityModal}>
+              <FiMenu/>
+            </div>
             <img src={power} className='logo'></img>
           </div>
           <div className='navbar-right'>
@@ -35,6 +34,8 @@ function NavBar () {
             <Link to={'/dish_survey'}>Dish Survey</Link>
             <button onClick={logoutUser}>Logout</button>
           </div>
+          {/* {communityModal && <CommunityModal communityModal={communityModal} setCommunityModal={setCommunityModal}/>} */}
+          <CommunityModal />
         </div>
       );
     } else {
@@ -42,15 +43,19 @@ function NavBar () {
         <div className="navbar">
           {/* <Link to={'/signup'}>Signup</Link>
           <Link to={'/login'}>Login</Link> */}
-          <div className='navbar-left' onClick={handleLogoClick}>
+          <div className='navbar-left'>
+            {/* <div className="community-menu-icon" onClick={openCommunityModal}>
+              <FiMenu/>
+            </div> */}
             <img src={power} className='logo'></img>
           </div>
           <div className='navbar-right'>
             <Link to={'/signup'}><button>Signup</button></Link>
             <Link to={'/login'}><button>Login</button></Link>
           </div>
-
-
+        
+        {/* {communityModal && <CommunityModal communityModal={communityModal} setCommunityModal={setCommunityModal}/>} */}
+        {/* <CommunityModal /> */}
         </div>
       );
     }
