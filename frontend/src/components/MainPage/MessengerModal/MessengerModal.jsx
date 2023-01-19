8399ec3e27327d6850540ed017399a050b496ee4
Max <mfong415@gmail.com>
2023-01-18 20:19:19 -0800

    added messenger stuff

--- frontend/src/components/MainPage/MessengerModal/MessengerModal.jsx
+++ frontend/src/components/MainPage/MessengerModal/MessengerModal.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
-
+import MessageForm from './MessegeForm'
 const MessengerModal = ({messengerModal, setMessengerModal}) => {
 
 
+      
+  if (messengerModal) document.body.classList.add('active-modal')
+  else document.body.classList.remove('active-modal')
+
   return (
-    <div className='messenger-univ' id='messenger-show-body'>
+        <div>
+            <div className='modal-overlay' onClick={()=>setMessengerModal(!messengerModal)}>
+                <div className='messenger-univ' id='messenger-show-body' onClick={e => e.stopPropagation()}>
+                <div id='messenger-log'>
 
 
+                </div>
+                <MessageForm/>
+             </div>
+        </div>
     </div>
   )
 }
