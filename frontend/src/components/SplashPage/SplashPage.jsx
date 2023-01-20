dfa95c8de98f045ef0c55ac47dbe88393975ef3d
Patrick Choi <ptrckchoi@gmail.com>
2023-01-20 09:28:35 -0800

    finished splash page

--- frontend/src/components/SplashPage/SplashPage.jsx
+++ frontend/src/components/SplashPage/SplashPage.jsx
@@ -4,14 +4,14 @@ import {login, logout} from '../../store/session';
 import egg_pan from '../../assets/egg_pan.png';
 import table from '../../assets/table_dinner.png';
 import mapsicon from '../../assets/maps-icon.png';
-
+import groupPNG from '../../assets/group_png.png';
 function SplashPage () {
 
     return (
         <div className="splash-page-container">
             <div className="splash-page-1 splash-page-component">
                 <div className="splash-page-1-left splash-text">
-                    <h1>Find the best food for you</h1>
+                    <h1>We help find the best food for you</h1>
                     <p>Welcome to Singed, a food recommendation app that helps find the best nearby foods and restaurants for you!</p>
                     {/* <div className="splash-page-buttons">
                         <Link to="/signup" className="signup-button">Sign Up</Link>
@@ -33,11 +33,21 @@ function SplashPage () {
             </div>
             <div className="splash-page-3 splash-page-component">
                 <div className="splash-page-3-left splash-text">
-                    <h1>Location ...</h1>
-                    <p>We use a Google Maps Api to ...</p>
+                    <h1>Interact with friends and make group orders</h1>
+                    <p>Using WebSockets, we connect users with their friends to chat and order together, 
+                        making recomendations based on their combined preferences</p>
                 </div>
                 <div className="splash-page-3-right splash-img">
+                    <img src={groupPNG}/>
+                </div>
+            </div>
+            <div className="splash-page-4 splash-page-component">
+                <div className="splash-page-4-left splash-img">
                     <img src={mapsicon}/>
+                </div>
+                <div className="splash-page-4-right splash-text">
+                    <h1>Location Services</h1>
+                    <p>We implement a Google Maps Api to locate the best restaurants close by</p>
                 </div>
             </div>
         </div>
