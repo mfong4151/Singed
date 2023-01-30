16730d429fb131ad2e0cd29eca1f8f3f30ee44b4
Max <mfong415@gmail.com>
2023-01-30 11:51:51 -0800

    Forgot the group items

--- frontend/src/components/NavBar/CommunityModal/MyGroupItem.jsx
+++ frontend/src/components/NavBar/CommunityModal/MyGroupItem.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-const MyGroupItem = () => {
+
+
+//preconditions: friend, should have an id, name, and picture?
+const MyGroupItem = ({group}) => {
+  
+
   return (
-    <div>MyGroupItem</div>
+    <li className='udc-right friend-list-item'>
+
+        {/* //Do we wnat profile pictures? made a placeholder for it */}
+
+        {groupMember.name}
+
+
+    </li>
   )
 }
 
