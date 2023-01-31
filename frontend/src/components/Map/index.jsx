bb623396d63e8453f542f5d78e6c6bcccebf84e5
ByronHan333 <zhan12@dons.usfca.edu>
2023-01-30 16:36:59 -0800

    added main page jsx

--- frontend/src/components/Map/index.jsx
+++ frontend/src/components/Map/index.jsx
@@ -6,16 +6,9 @@ import {styles} from './MapStyles'
 import { useHistory } from "react-router-dom";
 import {fetchRestaurantsCoordinatePreference} from '../../store/restaurant'
 
-export default function Map({preference}) {
-  const dispatch = useDispatch()
-  let restaurants = useSelector(state => state.restaurants)
-
+export default function Map({restaurants, preference}) {
   let lat =  37.779180920571605;
   let lng =  -122.42151230151367;
-
-  useEffect(() => {
-    dispatch(fetchRestaurantsCoordinatePreference({lat, lng, preference}))
-  },[dispatch])
 
   return <MapContainer restaurants={Object.values(restaurants)} center={{lat, lng}} preference={preference}/>
 }
@@ -40,15 +33,15 @@ export function MapContainer({restaurants, center, preference}) {
     mapId: '73cef3161f877bcd',
     disableDefaultUI:false,
     clickableIcons:false,
+  };
 
-};
-
-  const restaurantCardOnClick = (e, restaurant) => {
+  const restaurantCardOnClick = (e, restaurant, ref) => {
     e.preventDefault()
+    console.log(ref);
+    ref.current?.scrollIntoView({behavior: 'smooth'});
   }
 
   const mapOnClick = (e => {
-
     dispatch(fetchRestaurantsCoordinatePreference({lat: e.latLng.lat(), lng: e.latLng.lng(), preference}))
   })
 
@@ -56,7 +49,7 @@ export function MapContainer({restaurants, center, preference}) {
   return (
     <div className="map-div">
       <GoogleMap zoom={12} center={centerM} onClick={mapOnClick} mapContainerClassName="map-container" options={options}>
-        {restaurants.map(restaurant => {
+        {restaurants.map((restaurant, index) => {
           return (
             <OverlayView key = {restaurant._id} position={{lat:restaurant.latitude, lng:restaurant.longitude}} mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}>
               <RestaurantCard restaurant={restaurant} onClick={(e) => restaurantCardOnClick(e, restaurant)} />
