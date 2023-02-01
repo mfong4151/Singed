27852ec384db5121ef7cb4c7c9a1d6a0fdef4c76
ByronHan333 <zhan12@dons.usfca.edu>
2023-02-01 14:45:39 -0800

    added hover effect

--- frontend/src/components/Map/index.jsx
+++ frontend/src/components/Map/index.jsx
@@ -5,6 +5,7 @@ import './Map.css'
 import {styles} from './MapStyles'
 import { useHistory } from "react-router-dom";
 import {fetchRestaurantsCoordinatePreference} from '../../store/restaurant'
+import './RestaurantInfo.css'
 
 export default function Map({restaurants, preference}) {
   let lat =  37.779180920571605;
@@ -13,24 +14,38 @@ export default function Map({restaurants, preference}) {
   return <MapContainer restaurants={Object.values(restaurants)} center={{lat, lng}} preference={preference}/>
 }
 
-export function RestaurantCard({restaurant, onClick}) {
+export function RestaurantInfo({restaurant}) {
+  console.log("on mouse over")
   return (
-    <div className="map-price-card cursor" onClick={onClick}>
+    <div className='restaurant-hover-info'>
+      <img className="restaurant-hover-info-photo" src={restaurant.imageUrl} alt="" />
+      <p className="">{restaurant?.name}</p>
+      <p className="">{restaurant?.address}</p>
+      <p className="">Rating: {restaurant?.rating}</p>
+      <p className="">Type: {restaurant?.cuisine_type}</p>
+    </div>
+  )
+}
+
+export function RestaurantCard({restaurant, onClick}) {
+  const [condition, setCondition] = useState(false)
+  return (
+    <div className="map-price-card cursor" onClick={onClick} onMouseOver={()=>setCondition(true)} onMouseOut={()=>setCondition(false)}>
       <p>Score: {Math.round(restaurant.dotProduct*100)}</p>
+      {condition ? <RestaurantInfo restaurant={restaurant}/> : null}
     </div>
   )
 }
 
 export function MapContainer({restaurants, center, preference}) {
   const dispatch = useDispatch();
-  //The reason for the ternary operation is that render has issues when we use the ENV variable in render, 
+  //The reason for the ternary operation is that render has issues when we use the ENV variable in render,
   //so I've hard coded a key in here to show when process.ENV.(key) comes through as undefined
-  //Darrens suggestion with the FSP was that these had to be hard coded in anyways. 
-  
+  //Darrens suggestion with the FSP was that these had to be hard coded in anyways.
   const {isLoaded} = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_MAPS_API_KEY  ? process.env.REACT_APP_MAPS_API_KEY : 'AIzaSyASdAh77--unS806Dxb3JGAEJ1Vvl9j3ZY'
   });
-  
+
   const centerM = useMemo(() => (center), [])
 
   const options = {
@@ -52,7 +67,7 @@ export function MapContainer({restaurants, center, preference}) {
   if (!isLoaded) return <div>Loading...</div>
   return (
     <div className="map-div">
-      <GoogleMap zoom={12} center={centerM} onClick={mapOnClick} mapContainerClassName="map-container" options={options}>
+      <GoogleMap zoom={14} center={centerM} onClick={mapOnClick} mapContainerClassName="map-container" options={options}>
         {restaurants.map((restaurant, index) => {
           return (
             <OverlayView key = {restaurant._id} position={{lat:restaurant.latitude, lng:restaurant.longitude}} mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}>
