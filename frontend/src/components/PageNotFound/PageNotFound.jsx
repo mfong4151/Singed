21da1f72e80619b8390319ac95d19d5f2662fe88
Max <mfong415@gmail.com>
2023-02-01 19:02:23 -0800

    Done with refactoring redirect from page not found

--- frontend/src/components/PageNotFound/PageNotFound.jsx
+++ frontend/src/components/PageNotFound/PageNotFound.jsx
@@ -8,7 +8,7 @@ function PageNotFound () {
             <div className="pagenotfound-left">
                 <h1>404</h1>
                 <h2>Oops, we can't find what you're looking for</h2>
-                <Link to="/" className="pagenotfound-homebutton">Back To Singed</Link>
+                <Link to="/main" className="pagenotfound-homebutton">Back To Singed</Link>
             </div>
             <div className="pagenotfound-right">
                 <Link to="/attributions">
