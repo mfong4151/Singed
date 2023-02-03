4fa586ee0c63ca2ef93b2b39e3a92a95b5fa8672
Max <mfong415@gmail.com>
2023-02-03 05:59:42 -0800

    Added a fix to scores over 100 showing

--- frontend/src/components/Map/index.jsx
+++ frontend/src/components/Map/index.jsx
@@ -26,11 +26,19 @@ export function RestaurantInfo({restaurant}) {
   )
 }
 
+const reduceScore = (score) =>{
+  while(score > 100){
+    score/= 2
+  }
+  
+  return Math.floor(score)
+}
+
 export function RestaurantCard({restaurant, onClick}) {
   const [condition, setCondition] = useState(false)
   return (
     <div className="map-price-card cursor" onClick={onClick} onMouseOver={()=>setCondition(true)} onMouseOut={()=>setCondition(false)}>
-      <p>Score: {Math.round(restaurant.dotProduct*100)}</p>
+      <p>Score: {Math.round(restaurant.dotProduct*100) > 100 ? reduceScore(Math.round(restaurant.dotProduct*100)): Math.round(restaurant.dotProduct*100)}</p>
       {condition ? <RestaurantInfo restaurant={restaurant}/> : null}
     </div>
   )
